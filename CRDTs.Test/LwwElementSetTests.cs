e425b1edf8b404fca5233638ed0e88bad2b36c08
mitake_ck <mitake.ck@gmail.com>
2022-03-10 10:25:54 +0900

    impl lww-e-set

---
+++ CRDTs.Test/LwwElementSetTests.cs
@@ -0,0 +1,192 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CRDTs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRDTs.Tests
+{
+    [TestClass()]
+    public class LwwElementSetTests
+    {
+        [TestMethod()]
+        public void LwwElementSetTest()
+        {
+            var set = new LwwElementSet<string>();
+
+            Assert.AreEqual(0, set.Values.Count);
+            Assert.IsFalse(set.Values.Contains("A"));
+        }
+
+        [TestMethod()]
+        public void AddTest()
+        {
+            var set = new LwwElementSet<string>();
+
+            set.Add("A");
+
+            Assert.AreEqual(1, set.Values.Count);
+            Assert.IsTrue(set.Values.Contains("A"));
+        }
+
+        [TestMethod()]
+        public void AddTest2()
+        {
+            var set = new LwwElementSet<string>();
+
+            set.Add("A", new TimeStamp(100));
+            set.Add("B", new TimeStamp(101));
+
+            Assert.AreEqual(2, set.Values.Count);
+            Assert.IsTrue(set.Values.Contains("A"));
+            Assert.IsTrue(set.Values.Contains("B"));
+            Assert.IsFalse(set.Values.Contains("C"));
+        }
+
+        [TestMethod()]
+        public void RemoveTest()
+        {
+            var set = new LwwElementSet<string>();
+
+            set.Add("A", new TimeStamp(100));
+            set.Add("B", new TimeStamp(101));
+            set.Remove("B", new TimeStamp(102));
+
+            Assert.AreEqual(1, set.Values.Count);
+            Assert.IsTrue(set.Values.Contains("A"));
+            Assert.IsFalse(set.Values.Contains("B"));
+            Assert.IsFalse(set.Values.Contains("C"));
+        }
+
+        [TestMethod()]
+        public void RemoveTest2()
+        {
+            var set = new LwwElementSet<string>();
+
+            set.Add("A", new TimeStamp(100));
+            set.Remove("B", new TimeStamp(100));
+            set.Add("B", new TimeStamp(101));
+
+            Assert.AreEqual(2, set.Values.Count);
+            Assert.IsTrue(set.Values.Contains("A"));
+            Assert.IsTrue(set.Values.Contains("B"));
+            Assert.IsFalse(set.Values.Contains("C"));
+        }
+
+        [TestMethod()]
+        public void RemoveTest3()
+        {
+            var set = new LwwElementSet<string>();
+
+            set.Add("A", new TimeStamp(100));
+            set.Remove("B", new TimeStamp(100));
+            set.Add("C", new TimeStamp(101));
+
+            Assert.AreEqual(2, set.Values.Count);
+            Assert.IsTrue(set.Values.Contains("A"));
+            Assert.IsFalse(set.Values.Contains("B"));
+            Assert.IsTrue(set.Values.Contains("C"));
+        }
+
+        [TestMethod()]
+        public void MergeTest()
+        {
+            var set1 = new LwwElementSet<string>();
+            var set2 = new LwwElementSet<string>();
+
+            set1.Add("A");
+            set2.Add("B");
+
+            set1.Merge(set2);
+
+            Assert.AreEqual(2, set1.Values.Count);
+            Assert.IsTrue(set1.Values.Contains("A"));
+            Assert.IsTrue(set1.Values.Contains("B"));
+            Assert.IsFalse(set1.Values.Contains("C"));
+        }
+
+        [TestMethod()]
+        public void MergeTest2()
+        {
+            var set1 = new LwwElementSet<string>();
+            var set2 = new LwwElementSet<string>();
+
+            set1.Add("A", new TimeStamp(100));
+            set2.Remove("A", new TimeStamp(101));
+
+            set1.Merge(set2);
+
+            Assert.AreEqual(0, set1.Values.Count);
+            Assert.IsFalse(set1.Values.Contains("A"));
+            Assert.IsFalse(set1.Values.Contains("B"));
+            Assert.IsFalse(set1.Values.Contains("C"));
+        }
+
+        [TestMethod()]
+        public void MergeTest3()
+        {
+            var set1 = new LwwElementSet<string>();
+            var set2 = new LwwElementSet<string>();
+
+            set1.Add("A", new TimeStamp(100));
+            set2.Remove("A", new TimeStamp(99));
+
+            set1.Merge(set2);
+
+            Assert.AreEqual(1, set1.Values.Count);
+            Assert.IsTrue(set1.Values.Contains("A"));
+            Assert.IsFalse(set1.Values.Contains("B"));
+            Assert.IsFalse(set1.Values.Contains("C"));
+        }
+
+        [TestMethod()]
+        public void MergeTest4()
+        {
+            var set1 = new LwwElementSet<string>();
+            var set2 = new LwwElementSet<string>();
+
+            set1.Add("A", new TimeStamp(100));
+            set1.Add("B", new TimeStamp(100));
+            set1.Add("C", new TimeStamp(100));
+
+            set2.Add("D", new TimeStamp(100));
+            set2.Remove("A", new TimeStamp(200));
+            set2.Remove("B", new TimeStamp(200));
+            set2.Remove("C", new TimeStamp(200));
+
+            set1.Merge(set2);
+
+            Assert.AreEqual(1, set1.Values.Count);
+            Assert.IsFalse(set1.Values.Contains("A"));
+            Assert.IsFalse(set1.Values.Contains("B"));
+            Assert.IsFalse(set1.Values.Contains("C"));
+            Assert.IsTrue(set1.Values.Contains("D"));
+        }
+
+        [TestMethod()]
+        public void MergeTest5()
+        {
+            var set1 = new LwwElementSet<string>();
+            var set2 = new LwwElementSet<string>();
+
+            set1.Add("A", new TimeStamp(100));
+            set1.Add("B", new TimeStamp(100));
+            set1.Add("C", new TimeStamp(100));
+
+            set2.Add("D", new TimeStamp(99));
+            set2.Remove("A", new TimeStamp(99));
+            set2.Remove("B", new TimeStamp(99));
+            set2.Remove("C", new TimeStamp(99));
+
+            set1.Merge(set2);
+
+            Assert.AreEqual(4, set1.Values.Count);
+            Assert.IsTrue(set1.Values.Contains("A"));
+            Assert.IsTrue(set1.Values.Contains("B"));
+            Assert.IsTrue(set1.Values.Contains("C"));
+            Assert.IsTrue(set1.Values.Contains("D"));
+        }
+    }
+}
--- CRDTs/LwwElementSet.cs
+++ CRDTs/LwwElementSet.cs
@@ -12,6 +12,9 @@ namespace CRDTs
 
         public TimeStamp(long? ts = null)
         {
+            if (ts <= 0)
+                throw new Exception();
+
             Value = ts ?? DateTime.UtcNow.Ticks;
         }
     }
@@ -21,12 +24,91 @@ namespace CRDTs
         public Dictionary<T, TimeStamp> Added { get; private set; }
         public Dictionary<T, TimeStamp> Removed { get; private set; }
 
+        public HashSet<T> Values => Added.Keys.ToHashSet();
+
         public LwwElementSet(Dictionary<T, TimeStamp>? added = null, Dictionary<T, TimeStamp>? removed = null)
         {
             Added = added ?? new();
             Removed = removed ?? new();
         }
 
+        public TimeStamp? Lookup(T element)
+        {
+            TimeStamp? addedTime = Added.GetValueOrDefault(element);
+            TimeStamp? removedTime = Removed.GetValueOrDefault(element);
 
+            if (addedTime != null && removedTime != null)
+            {
+                return new(Math.Max(addedTime.Value, removedTime.Value));
+            }
+
+            return addedTime ?? removedTime ?? null;
+        }
+
+        public void Add(T element, TimeStamp? timestamp = null)
+        {
+            TimeStamp target = timestamp ?? new();
+            TimeStamp? current = Lookup(element);
+
+            if (current == null)
+            {
+                Added.Add(element, target);
+                return;
+            }
+
+            if (current.Value < target.Value)
+            {
+                Added.Remove(element);
+                Removed.Remove(element);
+
+                Added.Add(element, target);
+                return;
+            }
+
+            if (current.Value >= target.Value)
+            {
+                // nothing todo
+                return;
+            }
+        }
+
+        public void Remove(T element, TimeStamp? timestamp = null)
+        {
+            TimeStamp target = timestamp ?? new();
+            TimeStamp? current = Lookup(element);
+
+            if (current == null)
+            {
+                Removed.Add(element, target);
+                return;
+            }
+
+            if (current.Value < target.Value)
+            {
+                Added.Remove(element);
+                Removed.Remove(element);
+
+                Removed.Add(element, target);
+                return;
+            }
+
+            if (current.Value >= target.Value)
+            {
+                // nothing todo
+                return;
+            }
+        }
+
+        public void Merge(LwwElementSet<T> other)
+        {
+            foreach (var (item, timestamp) in other.Added)
+            {
+                Add(item, timestamp);
+            }
+            foreach (var (item, timestamp) in other.Removed)
+            {
+                Remove(item, timestamp);
+            }
+        }
     }
 }
